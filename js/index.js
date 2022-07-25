590ee421ec8348d3270f9dde49f071829c392b2e
AxelMrak <axelmrak@gmail.com>
2022-07-24 23:19:44 -0300

    Add JS events for .products divs for the arrows will appears when mouse enter in each one

--- js/index.js
+++ js/index.js
@@ -1,30 +1,63 @@
-// EFFECT OF ARROWS IN DIV 
-// const sale = document.querySelector(`.sale`);
-// const dress = document.querySelector(`.dresses`);
-// const acc = document.querySelector(`.accesories`);
-// const arrow1 = document.querySelector(`.arrow1`);
-// const arrow2 = document.querySelector(`.arrow2`);
-// const arrow3 = document.querySelector(`.arrow3`);
+// SUBMIT NEWSLATTER
 
-// sale.addEventListener(`mouseover`, () => {
-//   arrow1.style.opacity="1";
-// });
-// sale.addEventListener(`mouseleave`, () => {
-//   arrow1.style.opacity="0";
-// })
-// dress.addEventListener(`mouseover`, () => {
-//   arrow2.style.opacity="1";
-// });
-// dress.addEventListener(`mouseleave`, () => {
-//   arrow2.style.opacity="0";
-// })
-// acc.addEventListener(`mouseover`, () => {
-//   arrow3.style.opacity="1";
-// });
-// acc.addEventListener(`mouseleave`, () => {
-//   arrow3.style.opacity="0";
-// });
-// END ARROWS EFECT
+const btn = document.querySelector(`.btn`);
+
+
+btn.addEventListener(`click`, ()=>{
+  if (confirm(`Do you want suscribe to our Newslatter?`) == true) {
+    const email = document.getElementById("email-field").value;
+    sessionStorage.setItem(`EmailTo`, `${email}`);
+    alert(`Thanks, the offers and news will send to ${email}`)
+  } else {
+    alert(`Ok, you can follow us in our social networks.`);
+  };
+});
+
+// END NEWSLATTER
+
+
+// EFFECT OF ARROWS IN DIV 
+const sale = document.querySelector(`.sale`);
+const dress = document.querySelector(`.dress`);
+const accesories = document.querySelector(`.accesories`);
+const saleArrow = document.querySelector(`.arrow-sale`);
+const dressArrow = document.querySelector(`.arrow-dress`);
+const accArrow = document.querySelector(`.arrow-acc`);
+
+// ALL THESE EVENT LISTENERS CAN BE RESUME IN A FUNCTION, BUT IS BETTER FOR UNDERSTAND THE FUNCTIONALITY IN EACH ONE.
+sale.addEventListener(`mouseenter`, e => {
+  sale.style.height="80%"
+  saleArrow.style.display="initial"
+});
+
+sale.addEventListener(`mouseleave`, () => {
+  sale.style.height="40%"
+  saleArrow.style.display="none"
+});
+
+dress.addEventListener(`mouseenter`, e => {
+  dress.style.height="80%"
+  dressArrow.style.display="initial"
+});
+
+dress.addEventListener(`mouseleave`, () => {
+  dress.style.height="40%"
+  dressArrow.style.display="none"
+});
+
+accesories.addEventListener(`mouseenter`, e => {
+  accesories.style.height="80%"
+  accArrow.style.display="initial"
+});
+
+accesories.addEventListener(`mouseleave`, () => {
+  accesories.style.height="40%"
+  accArrow.style.display="none"
+});
+
+// 
+
+
 // GOOGLE MAPS API
 
 function initMap() {
@@ -52,20 +85,4 @@ initMap();
 
 // END GOOGLE MAPS API
 
-// SUBMIT NEWSLATTER
-
-// const btn = document.querySelector(`.btn`);
-
-
-// btn.addEventListener(`click`, ()=>{
-//   if (confirm(`Do you want suscribe to our Newslatter?`) == true) {
-//     const email = document.getElementById("email-field").value;
-//     sessionStorage.setItem(`EmailTo`, `${email}`);
-//     alert(`Thanks, the offers and news will send to ${email}`)
-//   } else {
-//     alert(`Ok, you can follow us in our social networks.`);
-//   };
-// });
-
-// END NEWSLATTER
 
